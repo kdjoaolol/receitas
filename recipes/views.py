1972cc428b2e49af6a8c2f0acfb4be03f775d136
Joao Victor <kdjoaolol123@gmail.com>
2022-09-28 22:45:58 -0300

    search e header

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,3 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return HttpResponse("HOME")
-
-def contato(request):
-    return render(request, 'recipes/home.html', context={'nome': 'Joao Victor'}) # django ja busca na pasta templates
